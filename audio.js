17a6865c354f1c3b2f695fb448b09b293498b467
mvacjar <mvacjar@gmail.com>
2024-05-28 15:22:56 +0200

    Update index.html with lazy loading for images and audio loop

---
+++ audio.js
@@ -0,0 +1,45 @@
+const filterTypes = [
+  "highpass",
+  "lowpass",
+  "bandpass",
+  "lowshelf",
+  "highshelf",
+  "peaking",
+  "notch",
+];
+
+const paragraphEl = document.querySelector("#text-sound");
+const audioEl = document.querySelector("#song");
+
+const ctx = new AudioContext();
+
+const sourceNode = ctx.createMediaElementSource(audioEl);
+const gainNode = ctx.createGain();
+const filterNode = ctx.createBiquadFilter();
+
+// Connect the nodes
+sourceNode.connect(filterNode);
+filterNode.connect(gainNode);
+gainNode.connect(ctx.destination);
+
+let lastFilterType = null;
+
+paragraphEl.addEventListener("mouseenter", () => {
+  let randomFilterType;
+
+  do {
+    randomFilterType =
+      filterTypes[Math.floor(Math.random() * filterTypes.length)];
+  } while (randomFilterType === lastFilterType);
+
+  filterNode.type = randomFilterType;
+  filterNode.frequency.value = 1000;
+
+  lastFilterType = randomFilterType;
+
+  audioEl.play();
+});
+
+paragraphEl.addEventListener("mouseleave", () => {
+  audioEl.pause();
+});
