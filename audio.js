54e094873286dccddbb66e3a25a1b96c4d3917ac
mvacjar <mvacjar@gmail.com>
2024-05-29 10:03:37 +0200

    Update index.html with new styles and scripts

--- audio.js
+++ audio.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
  // Get references to the play button and audio player
  const playButton = document.getElementById('playButton');
  const audioPlayer = document.getElementById('audioPlayer');
@@ -17,3 +18,50 @@
  audioPlayer.addEventListener('ended', function() {
      playButton.textContent = '►';
  });
+=======
+const filterTypes = [
+  "highpass",
+  "lowpass",
+  "bandpass",
+  "lowshelf",
+  "highshelf",
+  "peaking",
+  "notch",
+];
+
+const paragraphEl = document.querySelector("#text-sound");
+const audioEl = document.querySelector("#song");
+
+const ctx = new AudioContext();
+
+const sourceNode = ctx.createMediaElementSource(audioEl);
+const gainNode = ctx.createGain();
+const filterNode = ctx.createBiquadFilter();
+
+// Connect the nodes
+sourceNode.connect(filterNode);
+filterNode.connect(gainNode);
+gainNode.connect(ctx.destination);
+
+let lastFilterType = null;
+
+paragraphEl.addEventListener("mouseenter", () => {
+  let randomFilterType;
+
+  do {
+    randomFilterType =
+      filterTypes[Math.floor(Math.random() * filterTypes.length)];
+  } while (randomFilterType === lastFilterType);
+
+  filterNode.type = randomFilterType;
+  filterNode.frequency.value = 1000;
+
+  lastFilterType = randomFilterType;
+
+  audioEl.play();
+});
+
+paragraphEl.addEventListener("mouseleave", () => {
+  audioEl.pause();
+});
+>>>>>>> 02eed4c (Update index.html with new styles and scripts)
